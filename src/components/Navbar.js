c69993a90f9dd378f9a3a5459a827d1530ec5c00
msakac <msakac@foi.hr>
2022-05-14 10:20:54 +0200

    Slaganje navigacije da bude dropdown na mobilnom prikazu

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import '../scss/components/navbar.scss'
+
+
+const Navbar = () => {
+const [clicked, setValue] = useState(false)
+
+  return (
+    <div className='header'>
+        <nav className='navbar'>
+            <div className='navbar-icon' onClick = {()=>setValue(!clicked)}>
+                <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+            </div>
+            <ul className={clicked ? 'navbar-menu.active':'navbar-menu'}>
+                <li>
+                    <a className='navbar-link' href="/">O nama</a>
+                </li>
+                <li>
+                    <a className='navbar-link' href="/">Projekti</a>
+                </li>
+                <li>
+                    <a className='navbar-link' href="/">
+                        <img className='navbar-logo' src={require("../pictures/logo.png")} alt="Početna"></img>
+                    </a>
+                </li>
+                <li>
+                    <a className='navbar-link' href="/">Objave</a>
+                </li>
+                <li>
+                    <a className='navbar-link' href="/">Kontakt</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+};
+
+export default Navbar
